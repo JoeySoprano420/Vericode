d088cf9306442a5a1b0f794102b95869650d1184
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-05-21 11:13:02 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,33 @@
-# main.py
 from lexer import Lexer
+from parser import Parser
+import sys
 
-source_code = '''
-init count = 10
-check count > 0 {
-    make process() {
-        await process()
-    }
-}
-'''
+if __name__ == "__main__":
+    with open("hello_world.vc", "r") as f:
+        code = f.read()
 
-lexer = Lexer(source_code)
-token = lexer.next_token()
-while token.type != TokenType.EOF:
-    print(token)
-    token = lexer.next_token()
+    lexer = Lexer(code)
+    parser = Parser(lexer)
+    ast = parser.parse()
+
+    import json
+
+    def print_ast(node, indent=0):
+        pad = "  " * indent
+        print(f"{pad}{node.node_type}")
+        for attr in vars(node):
+            if attr == "node_type":
+                continue
+            val = getattr(node, attr)
+            if isinstance(val, list):
+                for item in val:
+                    if isinstance(item, ASTNode):
+                        print_ast(item, indent + 1)
+                    else:
+                        print(f"{pad}  {attr}: {item}")
+            elif isinstance(val, ASTNode):
+                print_ast(val, indent + 1)
+            else:
+                print(f"{pad}  {attr}: {val}")
+
+    print_ast(ast)
